5270767afd945f7fb1238af2d1d43c78a119e831
sailordude919 <mason@mungoh.com>
2015-01-17 00:52:08 -0500

    Initial Commit

---
+++ DocFinder/DocFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DocFinder
+//
+//  Created by Mason Wolters on 1/17/15.
+//  Copyright (c) 2015 Big Head Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DocFinder/DocFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DocFinder
+//
+//  Created by Mason Wolters on 1/17/15.
+//  Copyright (c) 2015 Big Head Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

152b34da479c0ec494b0e26e8e8be3e7bd189ce0
sailordude919 <mason@mungoh.com>
2015-01-17 01:31:24 -0500

    twilio keys

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,6 +1,8 @@
+var twilio = require('twilio')('ACa7d329d1b5c88a9b2be8674ea4b304da', 'c307a90e5205a174cc2353de8fb4fa73');
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+

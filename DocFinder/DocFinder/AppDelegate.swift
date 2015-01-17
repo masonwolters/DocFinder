980613bc3024c66c7cbb10e45a6117f1f3197504
Russell Ladd <russell.ladd@grl5.com>
2015-01-17 01:54:26 -0500

    Added main view controller

--- DocFinder/DocFinder/AppDelegate.swift
+++ DocFinder/DocFinder/AppDelegate.swift
@@ -11,10 +11,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow!
+    
     func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         
         Parse.enableLocalDatastore()
         Parse.setApplicationId("CdmE4AzemBZxYoogBlCCaEOrnv3w9E3s8jGqaHtn", clientKey: "1pVBUqEJ90BwnwL7cjUGVgx9Cau2KaUaM6egj0u2")
+        
+        let mainViewController = MainViewController(nibName: "MainViewController", bundle: nil)
+        
+        window.rootViewController = mainViewController
         
         return true
     }

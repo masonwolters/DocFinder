27289fa02168456f026b1398a54a0b50c14a51ff
Russell Ladd <russell.ladd@grl5.com>
2015-01-17 01:16:46 -0500

    Added comment

--- DocFinder/DocFinder/AppDelegate.m
+++ DocFinder/DocFinder/AppDelegate.m
@@ -16,7 +16,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // Setup the first view controller
+    
     return YES;
 }
 

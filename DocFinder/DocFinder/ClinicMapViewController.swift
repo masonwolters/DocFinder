4cd117bc0315642c3c797ee946f06b5458b51ab9
sailordude919 <mason@mungoh.com>
2015-01-17 13:19:55 -0500

    Messing with swift

--- DocFinder/DocFinder/ClinicMapViewController.swift
+++ DocFinder/DocFinder/ClinicMapViewController.swift
@@ -17,29 +17,44 @@ class ClinicMapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.loadData()
+        self.loadClinics()
     }
     
     func refreshMap() {
-        
+        if let clinicsNearby = clinicsNearby {
+            for clinic in clinicsNearby {
+                var annotation = MKPointAnnotation()
+                var geoPoint = clinic.objectForKey("location") as PFGeoPoint
+                annotation.coordinate = CLLocationCoordinate2DMake(geoPoint.latitude, geoPoint.longitude)
+                mapView.addAnnotation(annotation)
+            }
+        }
     }
     
-    func loadData() {
+    func loadClinics() {
+        println("load data")
         
         var query = PFQuery(className: "Clinic")
         
-        LocationHelper.sharedInstance.getCurrentLocation({ (location: CLLocation) -> Void in
-            
-            var geoPoint = PFGeoPoint(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            query.whereKey("location", nearGeoPoint: geoPoint, withinKilometers: 100)
-            query.findObjectsInBackgroundWithBlock() { (results: [AnyObject]!, error: NSError!) in
-                self.clinicsNearby = results as? Array
-                self.refreshMap()
-            }
-            
-        }, error: { (error: NSError) -> Void in
-            
-        })
+        query.findObjectsInBackgroundWithBlock() { (results: [AnyObject]!, error: NSError!) in
+            self.clinicsNearby = results as? Array
+            println("clinics nearby count: \(self.clinicsNearby!.count)")
+            self.refreshMap()
+        }
+        
+//        LocationHelper.sharedInstance.getCurrentLocation({ (location: CLLocation) -> Void in
+//            
+//            var geoPoint = PFGeoPoint(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+////            query.whereKey("location", nearGeoPoint: geoPoint, withinKilometers: 100)
+//            query.findObjectsInBackgroundWithBlock() { (results: [AnyObject]!, error: NSError!) in
+//                self.clinicsNearby = results as? Array
+//                println("clinics nearby count: \(self.clinicsNearby!.count)")
+//                self.refreshMap()
+//            }
+//            
+//        }, error: { (error: NSError) -> Void in
+//            
+//        })
     }
     
 }

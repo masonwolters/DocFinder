afc12895301be354a77a309e9b996d06dfb7ec7c
Russell Ladd <russell.ladd@grl5.com>
2015-01-17 15:36:07 -0500

    Added icons Added more to doctor view controller

--- DocFinder/DocFinder/DoctorLogInViewController.swift
+++ DocFinder/DocFinder/DoctorLogInViewController.swift
@@ -25,17 +25,19 @@ class DoctorLogInViewController: UIViewController, PFLogInViewControllerDelegate
     func logInViewController(logInController: PFLogInViewController!, didLogInUser user: PFUser!) {
         
         dismissViewControllerAnimated(true) {
-            
-            let doctor = Doctor(object: user)
-            
-            let doctorViewController = DoctorViewController(doctor: doctor!)
-            doctorViewController.delegate = self
-            
-            self.showViewController(doctorViewController, sender: nil)
+            self.showDoctorViewController(user, animated: true)
         }
     }
     
-    // MARK: DoctorViewControllerDelegate
+    // MARK: DoctorViewController
+    
+    func showDoctorViewController(doctor: PFUser, animated: Bool) {
+        
+        let doctorViewController = DoctorViewController(doctor: doctor)
+        doctorViewController.delegate = self
+        
+        navigationController!.pushViewController(doctorViewController, animated: animated)
+    }
     
     func doctorViewControllerDidLogout(doctorViewController: DoctorViewController) {
         navigationController!.popToRootViewControllerAnimated(true)

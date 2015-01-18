d8e0ab17be6a3178a6d9db1f51fe7631d5f72152
Russell Ladd <russell.ladd@grl5.com>
2015-01-18 08:42:02 -0500

    Updated log in screen

--- DocFinder/DocFinder/AppDelegate.swift
+++ DocFinder/DocFinder/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
     let tabBarController = UITabBarController()
     
     let doctorNavigationController = UINavigationController()
-    let doctorLogInViewController = DoctorLogInViewController(nibName: "DoctorLogInViewController", bundle: nil)
+    let doctorLogInViewController = DoctorLogInViewController()
     
     let clinicNavigationController = UINavigationController()
     let clinicMapViewController = ClinicMapViewController(nibName: "ClinicMapViewController", bundle: nil)
--- DocFinder/DocFinder/DoctorLogInViewController.swift
+++ DocFinder/DocFinder/DoctorLogInViewController.swift
@@ -7,17 +7,119 @@
 //
 
 import UIKit
+import CoreGraphics
 
-class DoctorLogInViewController: UIViewController, PFLogInViewControllerDelegate, DoctorViewControllerDelegate {
+class DoctorLogInViewController: UIViewController, UITextFieldDelegate, DoctorViewControllerDelegate {
     
-    // MARK: Actions
+    // MARK: Initialization
+    
+    override init() {
+        
+        super.init(nibName: "DoctorLogInViewController", bundle: nil)
+        
+        navigationItem.title = "FindMD"
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    // MARK: View
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.layer.cornerRadius = 20.0
+        imageView.layer.minificationFilter = kCAFilterTrilinear
+        imageView.clipsToBounds = true
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillChangeFrame:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    // MARK: Log in button
+    
+    @IBOutlet var logInButton: UIButton!
     
     @IBAction func logInButtonTouchUpInside() {
         
-        let logInViewController = PFLogInViewController()
-        logInViewController.delegate = self
+        PFUser.logInWithUsernameInBackground(emailTextField.text, password: passwordTextField.text) { user, error in
+            
+            if let doctor = user {
+                
+                self.passwordTextField.text = ""
+                
+                self.showDoctorViewController(doctor, animated: true)
+                
+            } else {
+                
+                let alertController = UIAlertController(title: "Invalid Credentials", message: "We could not find an account matching that email and password.", preferredStyle: .Alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: { action in
+                    
+                }))
+                
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    // MARK: Image view
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    // MARK: Text fields
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBAction func textFieldEditingChanged() {
+        logInButton.enabled = !emailTextField.text.isEmpty && !passwordTextField.text.isEmpty
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
         
-        presentViewController(logInViewController, animated: true, completion: nil)
+        if textField === emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if logInButton.enabled {
+            logInButtonTouchUpInside()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return false
+    }
+    
+    // MARK: Keyboard
+    
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var bottomLayoutConstraint: NSLayoutConstraint!
+    
+    func keyboardWillChangeFrame(notification: NSNotification) {
+        
+        let frame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+        let duration = (notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as NSNumber).doubleValue
+        
+        let localFrame = containerView.convertRect(frame, fromView: nil)
+        let keyboardMinY = localFrame.minY
+        
+        let distance = containerView.bounds.height - keyboardMinY
+        
+        bottomLayoutConstraint.constant = max(distance, 0.0)
+        view.setNeedsUpdateConstraints()
+        
+        UIView.animateWithDuration(duration, delay: 0.0, options: nil, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    // MARK: Tap gesture recognizer
+    
+    @IBAction func tapGestureRecognizerAction() {
+        view.endEditing(true)
     }
     
     // MARK: PFLogInViewControllerDelegate
